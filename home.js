5c2a255a577201082eb1bb53fcd713fd2834b4e4
Dalton <daltonkaris2000@gmail.com>
2020-08-15 22:08:22 +0300

    Initial commit
    Tip calculator based on some parameters

---
+++ home.js
@@ -0,0 +1,31 @@
+const bill = document.querySelector("#bill");
+const option = document.querySelector("#option");
+const people = document.querySelector("#people");
+const btn = document.querySelector(".modal-close");
+const result = document.querySelector('#result');
+
+document.querySelector("#form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    let total = parseInt(bill.value);
+    let nopeople = parseInt(people.value);
+    let pricePerPerson = total / nopeople;
+    
+    let tip;
+
+    if (option.value == 'Excellent') {
+        tip = Math.ceil(pricePerPerson / 2);
+    }else if (option.value == 'Average') {
+        tip = Math.ceil(pricePerPerson / 3);
+    }else{
+        tip = Math.ceil(pricePerPerson / 4);
+    } 
+
+    const totalTip = tip * parseInt(nopeople);
+    result.append(`Total Tip: ${totalTip}`);
+    
+    bill.value = '';
+    option.value = '';
+    people.value = '';
+    
+});
